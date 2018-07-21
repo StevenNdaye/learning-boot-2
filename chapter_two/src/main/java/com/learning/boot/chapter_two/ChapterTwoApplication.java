59c3d90c663105ddb94238befdd074d1380b7f01
StevenNdaye <stevenndaye@gmail.com>
2018-07-21 12:44:49 +0200

    Added an html and css as well as a blocking image service

--- chapter_two/src/main/java/com/learning/boot/chapter_two/ChapterTwoApplication.java
+++ chapter_two/src/main/java/com/learning/boot/chapter_two/ChapterTwoApplication.java
@@ -2,11 +2,18 @@ package com.learning.boot.chapter_two;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class ChapterTwoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChapterTwoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChapterTwoApplication.class, args);
+    }
+
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
